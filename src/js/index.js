cdf87cb518da336d2ff30b2bbb99f97a187226c1
hayato hasegawa <58217100+y-i-t-m@users.noreply.github.com>
2020-05-24 00:04:54 +0900

    fileの整理

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import "../scss/style.scss";
 
-import test from './test';
+import test from './modules/test';
 import myname from "./modules/myname";
 console.log(test(3, 9));
 console.log(myname("長谷川"));
